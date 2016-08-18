cffa0dcf8a4f681ccae568ebdcb548974d6d97e5
songweifun <keepfun@163.com>
2016-08-19 01:12:47 +0800

    租赁列表页 展示页布局

---
+++ Apps/Index/Controller/ShowController.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 16/8/18
+ * Time: 下午2:12
+ */
+namespace Index\Controller;
+use Think\Controller;
+class ShowController extends Controller{
+    public function index(){
+        $this->display();
+    }
+}
